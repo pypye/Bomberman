8598986fc0c4392c758162535916aa78ddde9396
Duc Tran <20020077@vnu.edu.vn>
2021-12-17 10:13:46 +0700

    hot fix

--- src/main/java/ui/gui/game/AnnouncementGui.java
+++ src/main/java/ui/gui/game/AnnouncementGui.java
@@ -22,9 +22,10 @@ public class AnnouncementGui {
     private final TextGui levelText, timeText;
     private final ButtonGui btn1;
     private final ButtonGui btn2;
+    int nextLevel;
 
     public AnnouncementGui(boolean win) {
-        if(win){
+        if (win) {
             AudioManager.bgm.stop();
             AudioManager.victory.play();
         } else {
@@ -34,30 +35,30 @@ public class AnnouncementGui {
         SceneController.getCurrentScene().setActive(false); //pause game
         SystemInput.setActive(false); //pause input
         background = new ImageGui(new Vector2f(Config.WIDTH / 2f - 550, Config.HEIGHT / 2f - 300), new Vector2f(1100, 600), "Textures/Menu/announcement_background.png");
-        int nextLevel = SceneController.getCurrentScene().getLevel() + 1;
+        this.nextLevel = SceneController.getCurrentScene().getLevel() + 1;
         int timeElapsed = (int) InfoGuiList.getTimeCount();
-        if (nextLevel - 1 > Config.LEVEL_PLAYED) {
-            Config.LEVEL_PLAYED = nextLevel - 1;
+        if (this.nextLevel - 1 > Config.LEVEL_PLAYED) {
+            Config.LEVEL_PLAYED = this.nextLevel - 1;
             Config.TIME_PLAYED = timeElapsed;
-        } else if (nextLevel - 1 == Config.LEVEL_PLAYED) {
+        } else if (this.nextLevel - 1 == Config.LEVEL_PLAYED) {
             if (timeElapsed < Config.TIME_PLAYED) {
                 Config.TIME_PLAYED = timeElapsed;
             }
         }
         if (win) {
             image = new ImageGui(new Vector2f(Config.WIDTH / 2f - 500, Config.HEIGHT / 2f - 160), new Vector2f(1000, 414), "Textures/Menu/win.png");
-            levelText = new TextGui("Level: " + (nextLevel - 1), ColorRGBA.White);
+            levelText = new TextGui("Level: " + (this.nextLevel - 1), ColorRGBA.White);
             timeText = new TextGui("Time Elapsed: " + timeElapsed + "s", ColorRGBA.White);
             LocationGui.centerXObject(levelText, background, Config.HEIGHT / 2f - 100);
             LocationGui.centerXObject(timeText, background, Config.HEIGHT / 2f - 130);
-            btn1 = new ButtonGui("Next to level " + nextLevel, new Vector2f(Config.WIDTH / 2f - 210, Config.HEIGHT / 2f - 220), new Vector2f(200, 50)) {
+            btn1 = new ButtonGui("Next to level " + this.nextLevel, new Vector2f(Config.WIDTH / 2f - 210, Config.HEIGHT / 2f - 220), new Vector2f(200, 50)) {
                 @Override
                 public void onClick() {
-                    Debugger.log(Debugger.EVENT, "Win next accepted. Next level = " + nextLevel);
+                    Debugger.log(Debugger.EVENT, "Win next accepted. Next level = " + AnnouncementGui.this.nextLevel);
                     if (SceneController.getCurrentScene() instanceof Game) {
-                        SceneController.setScene(new Game(nextLevel));
+                        SceneController.setScene(new Game(AnnouncementGui.this.nextLevel));
                     } else if (SceneController.getCurrentScene() instanceof GameAI) {
-                        SceneController.setScene(new GameAI(nextLevel));
+                        SceneController.setScene(new GameAI(AnnouncementGui.this.nextLevel));
                     }
                     this.remove();
 
